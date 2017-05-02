fc4a864d5b7db7d854adc9a3c73d2d6966f0166c
patrick.purcell <patrick.purcell@gexaenergy.com>
2017-05-02 14:23:51 -0500

    Added login component and route

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
