51fe5ac8ea11428472982402ff6f44f0102c571b
Geetha Ramu <geetha.ramu@gexa.gexaenergy.com>
2017-05-25 09:18:16 -0500

    created message-center component

---
+++ src/app/authenticated/my-account/message-center/message-center.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mygexa-message-center',
+  templateUrl: './message-center.component.html',
+  styleUrls: ['./message-center.component.scss']
+})
+export class MessageCenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
