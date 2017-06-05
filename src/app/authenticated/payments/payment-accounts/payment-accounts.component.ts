680b65c9be268853a1020e89e6406e70be616a14
James Allen <james.allen@gexaenergy.com>
2017-06-05 16:00:55 -0500

    Saving temp.

--- src/app/authenticated/payments/payment-accounts/payment-accounts.component.ts
+++ src/app/authenticated/payments/payment-accounts/payment-accounts.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import * as $ from 'jquery';
+
+declare var jQuery: $;
 
 @Component({
   selector: 'mygexa-payment-accounts',
   templateUrl: './payment-accounts.component.html',
   styleUrls: ['./payment-accounts.component.scss']
 })
-export class PaymentAccountsComponent implements OnInit {
+export class PaymentAccountsComponent implements OnInit, AfterViewInit {
+  @ViewChild('modal_delete') modal_delete;
 
   constructor() { }
 
   ngOnInit() { }
+
+  ngAfterViewInit() { }
+
+  delete($event) {
+
+    if ($event && $event.preventDefault) { $event.preventDefault(); }
+
+    try {
+      jQuery(this.modal_delete.nativeElement).modal('show');
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
 }
