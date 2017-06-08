415a653c85b275e0895366afb223ddf088652eb7
James Allen <james.allen@gexaenergy.com>
2017-06-08 17:25:52 -0500

    Added the ability to add friends; added required global style.

--- src/app/validators/validator.ts
+++ src/app/validators/validator.ts
@@ -3,21 +3,20 @@
 */
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
- 
- export function validateEmail(c: FormControl) {
-      let EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return EMAIL_REGEXP.test(c.value) ? null : {
-        validateEmail: {
-          valid: false
-        }
-      }
+export function validateEmail(c: FormControl) {
+  const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return EMAIL_REGEXP.test(c.value) ? null : {
+    validateEmail: {
+      valid: false
     }
+  };
+}
 
 // FORM GROUP VALIDATORS
-export function equalityCheck(emailKey : string, confirmEmailKey : string){
-    return (group: FormGroup): {[key: string]: any} => {
-    let email = group.controls[emailKey];
-    let confirmEmail = group.controls[confirmEmailKey];
+export function equalityCheck (emailKey: string, confirmEmailKey: string) {
+  return (group: FormGroup): {[key: string]: any} => {
+    const email = group.controls[emailKey];
+    const confirmEmail = group.controls[confirmEmailKey];
     if (email.value !== confirmEmail.value) {
       return {
         equalityCheck: {
@@ -25,6 +24,5 @@ export function equalityCheck(emailKey : string, confirmEmailKey : string){
         }
       };
     }
-  }
-
+  };
 }
