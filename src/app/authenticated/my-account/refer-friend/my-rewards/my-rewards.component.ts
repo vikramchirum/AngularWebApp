a7e2b428bcd3ec6a5c27053c753d58aba8caeb2b
James Allen <james.allen@gexaenergy.com>
2017-06-09 15:18:27 -0500

    Utilizing and extending the service account selector.

--- src/app/authenticated/my-account/refer-friend/my-rewards/my-rewards.component.ts
+++ src/app/authenticated/my-account/refer-friend/my-rewards/my-rewards.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BillingAccount, BillingAccountService } from 'app/core/BillingAccount';
+
 @Component({
   selector: 'mygexa-my-rewards',
   templateUrl: './my-rewards.component.html',
   styleUrls: ['./my-rewards.component.scss']
 })
 export class MyRewardsComponent implements OnInit {
-  streetAddress: string;
-  city: string;
-  state: string;
-  zip: number;
-  addressEditing: boolean;
 
-  constructor() {
+  BillingAccount: BillingAccount = null;
+  addressEditing: boolean;
+  serviceChangedId: string = null;
+
+  constructor(
+    private BillingAccountService: BillingAccountService
+  ) {
     this.addressEditing = false;
   }
 
+  serviceChanged(Id) {
+    this.serviceChangedId = Id;
+  }
+
+  serviceUse() {
+    this.BillingAccountService
+      .getBillingAccount(this.serviceChangedId)
+      .then(BillingAccount => this.BillingAccount = BillingAccount);
+    this.addressEditing = !this.addressEditing;
+  }
+
   ngOnInit() {
-    this.streetAddress = "20455 - TX-249 #200,";
-    this.city = "Houston,";
-    this.state = "TX";
-    this.zip = 77070;
+    this.BillingAccountService
+      .getCurrentBillingAccount()
+      .then(BillingAccount => this.BillingAccount = BillingAccount);
   }
 
   toggleAddressEdit($event){
