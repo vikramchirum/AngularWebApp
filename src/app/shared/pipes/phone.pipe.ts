638f4aabef5848bba5249a158b0371e0db791f0c
Geetha Ramu <geetha.ramu@gexa.gexaenergy.com>
2017-06-14 06:40:27 -0500

    DIGITAL-27 added confirmation text on form submission and removed service address for message cnter

---
+++ src/app/shared/pipes/phone.pipe.ts
@@ -0,0 +1,40 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'PhonePipe'
+})
+export class PhonePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    if(typeof value !== 'undefined'){
+    var viewVal = value.trim().replace(/^\+/, '');
+    viewVal = viewVal.replace(/[^0-9]/g, '').slice(0,10);
+    var area, number;
+
+    switch(viewVal.length) {
+      case 1:
+      case 2:
+      case 3:
+        area = viewVal;
+        break;
+      
+      default:
+        area = viewVal.slice(0, 3);
+        number = viewVal.slice(3);
+    }
+    if(number){
+      if(number.length > 3) {
+        number = number.slice(0,3) + '-' +  number.slice(3,7);
+      }
+      else {
+        number = number;
+      }
+      return ("(" + area + ")" + number).trim().slice(0,13);
+    }
+    else{
+      return "(" + area;
+    }
+  }
+  }
+
+}
