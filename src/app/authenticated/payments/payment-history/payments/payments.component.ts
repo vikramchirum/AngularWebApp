03fa105896b5243386bb6d084a7df57a2c4979bd
James Allen <james.allen@gexaenergy.com>
2017-06-16 09:25:08 -0500

    Refactored the Payment History Payments table; added a mock data generator for payments.

--- src/app/authenticated/payments/payment-history/payments/payments.component.ts
+++ src/app/authenticated/payments/payment-history/payments/payments.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import MockData from './payments-data.json';
+import MockData from './payments.mock-data.json';
+
+interface IHistoryPayment {
+  Id: string;
+  date: Date;
+  amount: number;
+  status: string;
+  method: string;
+  account: string;
+}
 
 @Component({
   selector: 'mygexa-payment-history-payments',
@@ -9,12 +18,87 @@ import MockData from './payments-data.json';
 })
 export class PaymentsComponent implements OnInit {
 
-  private paymentData: Array<any>;
+  Payments: IHistoryPayment[] = [];
+  PaymentsSortingBy: string;
+  PaymentsPage: number;
+  PaymentsPerPage = 10;
 
-  constructor() { }
-
-  ngOnInit() {
-    this.paymentData = MockData;
+  private PaymentsSortingByLesser: number = null;
+  private PaymentsSortingByGreater: number = null;
+  set PaymentsSortingByDesc(desc: boolean) {
+    if (desc) {
+      this.PaymentsSortingByLesser = 1;
+      this.PaymentsSortingByGreater = -1;
+    } else {
+      this.PaymentsSortingByLesser = -1;
+      this.PaymentsSortingByGreater = 1;
+    }
   }
+  get PaymentsSortingByDesc(): boolean {
+    return this.PaymentsSortingByGreater === -1;
+  }
+
+  private sorting(a: IHistoryPayment, b: IHistoryPayment): number {
+    if (a[this.PaymentsSortingBy] < b[this.PaymentsSortingBy]) {
+      return this.PaymentsSortingByLesser;
+    }
+    if (a[this.PaymentsSortingBy] > b[this.PaymentsSortingBy]) {
+      return this.PaymentsSortingByGreater;
+    }
+    return 0;
+  }
+
+  get currentPageOfPayments(): IHistoryPayment[] {
+    const sorted = this.Payments.sort((a, b) => this.sorting(a, b));
+    const index = this.PaymentsPage * this.PaymentsPerPage;
+    const extent = index + this.PaymentsPerPage;
+    if (extent > sorted.length) {
+      return sorted.slice(index);
+    }
+    return sorted.slice(index, extent);
+  }
+
+  get currentPageNumber(): number {
+    return this.PaymentsPage + 1;
+  }
+  get totalPages(): number {
+    return Math.ceil(this.Payments.length / this.PaymentsPerPage);
+  }
+
+  nextPage(): void {
+    this.PaymentsPage++;
+  }
+
+  gotoPage(index: number): void {
+    this.PaymentsPage = index;
+  }
+
+  previousPage(): void {
+    this.PaymentsPage--;
+  }
+
+  sortBy(attribute: string): void {
+    if (this.PaymentsSortingBy === attribute) {
+      this.PaymentsSortingByDesc = !this.PaymentsSortingByDesc;
+    } else {
+      this.PaymentsSortingBy = attribute;
+      this.PaymentsSortingByDesc = true;
+    }
+  }
+
+  constructor() {
+    this.PaymentsPage = 0;
+    this.PaymentsSortingBy = 'date';
+    this.PaymentsSortingByDesc = true;
+    this.Payments = MockData;
+    // Process the mock data:
+    for (const index in this.Payments) {
+      if (this.Payments[index]) {
+        this.Payments[index].date = new Date(this.Payments[index].date);
+      }
+    }
+  }
+
+  ngOnInit() {}
 
 }
