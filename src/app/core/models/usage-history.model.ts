3c314b614e9721bf2c58d8ce490d73ca77bf5339
Geetha Ramu <geetha.ramu@gexa.gexaenergy.com>
2017-06-22 07:49:52 -0500

    Added usage-history service to updated Home page Usage History chart based on api data

---
+++ src/app/core/models/usage-history.model.ts
@@ -0,0 +1,9 @@
+export interface UsageHistory {
+    Billing_Account_Id: number,
+    Usage_Month: string,
+    Usage_Start_Date: string,
+    Usage_End_Date: string,
+    Usage_Type:string,
+    Usage:string,
+    Usage_Uom:string
+}
