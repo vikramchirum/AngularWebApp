63008b836448c280b588369e814aca5bff11d5ba
anusha.madabhushi <anusha.madabhushi@gexaenergy.com>
2017-06-22 17:25:35 -0500

    Invoice integration

--- src/app/authenticated/payments/components/current-charges.component.ts
+++ src/app/authenticated/payments/components/current-charges.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from 'app/core/invoiceservice.service';
 import { IBill } from 'app/core/models/bill.model';
+import { IBillLineItem } from 'app/core/models/billlineitem.model';
+
 import { Bill, BillService } from 'app/core/Bill';
 
 import { filter } from 'lodash';
@@ -11,45 +13,38 @@ import { filter } from 'lodash';
   styleUrls: ['./current-charges.component.scss']
 })
 export class CurrentChargesComponent implements OnInit {
-  all_bills: IBill[]; error: string = null;
+  all_bills: IBill[];
+  error: string = null;
+  public req_bill: IBill[];
   Bill: Bill = null;
-  public billing_acoount_id: number;
+  public billing_account_id: number;
   constructor(
     private BillService: BillService,
     private invoice_service: InvoiceService
   ) {
-    this.billing_acoount_id = 830688;
+    this.billing_account_id = 1408663;
   }
 
   ngOnInit() {
 
-    this.invoice_service.getBills(this.billing_acoount_id)
+    this.invoice_service.getBills(this.billing_account_id)
       .subscribe(
         response => {
           this.all_bills = response;
+          const currentDate = new Date(), y = currentDate.getFullYear() - 1, m = currentDate.getMonth(); // remove -1 to get current year data
+          const firstDay = new Date(y, m, 1);
+          const lastDay = new Date(y, m + 1, 0);
+          this.req_bill = filter(this.all_bills, bill => ( bill.Invoice_Date >= firstDay  && bill.Invoice_Date <= lastDay ));
+          debugger;
+          sessionStorage.setItem('invoice_id', this.req_bill[0].Invoice_Id.toString());
+          sessionStorage.setItem('invoice_date', this.req_bill[0].Invoice_Date.toString());
 
-          var currentDate = new Date(), y = currentDate.getFullYear(), m = currentDate.getMonth();
-          var firstDay = new Date(y, m, 1);
-          var lastDay = new Date(y, m + 1, 0);
-          const metBills = filter(this.all_bills, bill => {
-            var req_invoice_date = new Date(bill.invoice_date);
-            return ( firstDay <= req_invoice_date && req_invoice_date >= lastDay );
-          });
-
-          for ( var i = 0; i < this.all_bills.length; i++) {
-            var bill = this.all_bills[i];
-            var invoice_date =  bill.invoiceDate;
-
-          }
-
-          console.log('Bills:', this.all_bills);
         },
         error => {
           this.error = error.Message;
         }
 
       );
-
     this.BillService.getCurrentBill()
       .then((Bill: Bill) => this.Bill = Bill);
   }
--- src/app/authenticated/payments/components/view-bill.component.ts
+++ src/app/authenticated/payments/components/view-bill.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { InvoiceService } from 'app/core/invoiceservice.service';
+import { IBillLineItem } from 'app/core/models/billlineitem.model';
+import { IBill } from 'app/core/models/bill.model';
 
 import { Bill, BillService } from 'app/core/Bill';
+import { filter, forEach } from 'lodash';
 
 @Component({
   selector: 'mygexa-view-bill',
@@ -9,7 +13,18 @@ import { Bill, BillService } from 'app/core/Bill';
 })
 export class ViewBillComponent implements OnInit {
   @Input() bill_Id: string;
+  @Input() bill_object: IBill;
 
+  error: string = null;
+  public  bill_item_details: IBillLineItem[] = [];
+  public  bill_item_details_gexa_charges: IBillLineItem[] = [];
+  public  bill_item_details_other_charges: IBillLineItem[] = [];
+  public  bill_item_details_TDU_charges: IBillLineItem[] = [];
+  public  bill_item_details_tax: IBillLineItem[] = [];
+
+
+  public invoice_num: number;
+  public invoice_date: Date;
   bill: Bill = null;
   private openCharges = [];
 
@@ -19,7 +34,9 @@ export class ViewBillComponent implements OnInit {
    * @returns {number}
    */
   private subtotal(charge: any) {
-    return Bill.subtotal(charge);
+    let total = 0;
+    forEach(charge, item => total += item['Amount']);
+    return total;
   }
 
   /**
@@ -45,22 +62,36 @@ export class ViewBillComponent implements OnInit {
   }
 
   constructor(
-    private BillService: BillService
+    private BillService: BillService,
+    private invoice_service: InvoiceService
   ) { }
 
   ngOnInit() {
-    // If we've been provided an Id, use it.
-    if (this.bill_Id) {
-      // Get the specified bill.
-      this.BillService.getBill(this.bill_Id)
-        .then((bill: Bill) => this.bill = bill)
-        .catch((err: any) => console.log(err));
-    } else {
-      // Otherwise, get the current bill.
-      this.BillService.getCurrentBill()
-        .then((bill: Bill) => this.bill = bill)
-        .catch((err: any) => console.log(err));
-    }
+    this.invoice_num = Number(this.bill_object.Invoice_Id);
+    this.invoice_date = new Date(this.bill_object.Invoice_Date)
+    this.invoice_service.getItemizedBillDetails(this.invoice_num)
+      .subscribe(
+        response => {
+          this.bill_item_details = response;
+          this.bill_item_details_TDU_charges = filter(this.bill_item_details, item => (item.Bill_Line_Item_Type === 'TDSP'));
+          this.bill_item_details_gexa_charges = filter(this.bill_item_details, item => (item.Bill_Line_Item_Type === 'GEXA' && item.Bill_Line_Item_Sub_Type === 'Energy'));
+          this.bill_item_details_other_charges = filter(this.bill_item_details, item => (item.Bill_Line_Item_Type === 'GEXA' && item.Bill_Line_Item_Sub_Type === 'None'));
+          this.bill_item_details_tax = filter(this.bill_item_details, item => (item.Bill_Line_Item_Type === 'TAX'));
+        }
+      );
+
+    //If we've been provided an Id, use it.
+    // if (this.bill_Id) {
+    //   // Get the specified bill.
+    //   this.BillService.getBill(this.bill_Id)
+    //     .then((bill: Bill) => this.bill = bill)
+    //     .catch((err: any) => console.log(err));
+    // } else {
+    //   // Otherwise, get the current bill.
+    //   this.BillService.getCurrentBill()
+    //     .then((bill: Bill) => this.bill = bill)
+    //     .catch((err: any) => console.log(err));
+    // }
   }
 
 }
