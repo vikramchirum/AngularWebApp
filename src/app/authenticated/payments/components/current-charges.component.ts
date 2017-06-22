7b088c8fa31e30fff6620cfa58f60057b958af4f
anusha.madabhushi <anusha.madabhushi@gexaenergy.com>
2017-06-22 12:44:16 -0500

    Current charges integration- Added new function to sort obj array by date

--- src/app/authenticated/payments/components/current-charges.component.ts
+++ src/app/authenticated/payments/components/current-charges.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { InvoiceService } from 'app/core/invoiceservice.service';
+import { IBill } from 'app/core/models/bill.model';
 import { Bill, BillService } from 'app/core/Bill';
+
+import { filter } from 'lodash';
 
 @Component({
   selector: 'mygexa-current-charges',
@@ -8,16 +11,46 @@ import { Bill, BillService } from 'app/core/Bill';
   styleUrls: ['./current-charges.component.scss']
 })
 export class CurrentChargesComponent implements OnInit {
-
+  all_bills: IBill[]; error: string = null;
   Bill: Bill = null;
-
+  public billing_acoount_id: number;
   constructor(
-    private BillService: BillService
-  ) { }
+    private BillService: BillService,
+    private invoice_service: InvoiceService
+  ) {
+    this.billing_acoount_id = 830688;
+  }
 
   ngOnInit() {
+
+    this.invoice_service.getBills(this.billing_acoount_id)
+      .subscribe(
+        response => {
+          this.all_bills = response;
+
+          var currentDate = new Date(), y = currentDate.getFullYear(), m = currentDate.getMonth();
+          var firstDay = new Date(y, m, 1);
+          var lastDay = new Date(y, m + 1, 0);
+          const metBills = filter(this.all_bills, bill => {
+            var req_invoice_date = new Date(bill.invoice_date);
+            return ( firstDay <= req_invoice_date && req_invoice_date >= lastDay );
+          });
+
+          for ( var i = 0; i < this.all_bills.length; i++) {
+            var bill = this.all_bills[i];
+            var invoice_date =  bill.invoiceDate;
+
+          }
+
+          console.log('Bills:', this.all_bills);
+        },
+        error => {
+          this.error = error.Message;
+        }
+
+      );
+
     this.BillService.getCurrentBill()
       .then((Bill: Bill) => this.Bill = Bill);
   }
-
 }
--- src/app/core/user.service.ts
+++ src/app/core/user.service.ts
@@ -36,15 +36,15 @@ export class UserService implements CanActivate {
 
   constructor(private router: Router, private _http: Http) {
 
-     // this.loginUrl = 'http://localhost:53342/api/user/authentication';
-     // this.registerUrl = 'http://localhost:53342/api/user/register';
-     // this.getSecQuestionUrl = 'http://localhost:53342/api/user/getSecQues';
+     this.loginUrl = 'http://localhost:53342/api/user/authentication';
+     this.registerUrl = 'http://localhost:53342/api/user/register';
+     this.getSecQuestionUrl = 'http://localhost:53342/api/user/getSecQues';
 
     this.secQuesUrl = environment.Api_Url + "/user/securityQues";
-    this.getSecQuestionUrl = environment.Api_Url + "/user/getSecQues";
-    this.loginUrl = environment.Api_Url + "/user/authentication";
-    this.registerUrl = environment.Api_Url + "/user/register";
-  }
+  //   this.getSecQuestionUrl = environment.Api_Url + "/user/getSecQues";
+  //   this.loginUrl = environment.Api_Url + "/user/authentication";
+  //   this.registerUrl = environment.Api_Url + "/user/register";
+   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.verify_login(state.url);
@@ -90,7 +90,7 @@ export class UserService implements CanActivate {
     return this._http.post(this.loginUrl, urlSearchParams.toString(), requestOptions)
       .map((response: Response) => {
         var result = response.json();
-        console.log("Result:", result.Token);
+        console.log("Result:", result);
         const token = result.Token;
         if (token && token.length) {
           localStorage.setItem('gexa_auth_token', token);
