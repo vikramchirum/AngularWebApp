39c5373bdebc9dc0628da9317ee1a32b82ae2588
James Allen <james.allen@gexaenergy.com>
2017-06-23 14:43:59 -0500

    Adding the observable to the User service - with Mongo data fetching. - Appropriating the login component with the new user service. - Appropriating the customer account service with the new user service.

--- src/app/guest/login/login.component.ts
+++ src/app/guest/login/login.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {UserService} from "../../core/user.service";
-import {Router} from "@angular/router";
-import {IUser, ISecurityQuestions} from "./register";
-import {IRegisteredUser} from "./registeredUserDetails";
-import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
-import {validateEmail, equalCheck, validateInteger} from "app/validators/validator";
+import { Router } from '@angular/router';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+
+import { UserService } from 'app/core/user.service';
+import { IUser, ISecurityQuestions } from './register';
+import { equalCheck, validateEmail, validateInteger } from 'app/validators/validator';
 
 @Component({
   templateUrl: './login.component.html',
@@ -12,50 +12,49 @@ import {validateEmail, equalCheck, validateInteger} from "app/validators/validat
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
-  processing: boolean;
-  registerForm: FormGroup;
 
-  formSubmitted: boolean = false;
-   public user: IUser;
-  user_name: string;
+  processing: boolean = null;
+  registerForm: FormGroup = null;
+  formSubmitted: boolean = null;
+  user: IUser = null;
+  user_name: string = null;
   error: string = null;
-  password: string;
-  testdata: any;
-  secQuesArray: ISecurityQuestions[];
-  constructor(private user_service: UserService, private router: Router, private fb: FormBuilder) {
+  password: string = null;
+  secQuesArray: ISecurityQuestions[] = [];
+
+  constructor(
+    private UserService: UserService,
+    private Router: Router,
+    private FormBuilder: FormBuilder
+  ) {
     this.processing = false;
     this.registerForm = this.registerFormInit();
-  }
 
+  }
 
   login() {
     this.processing = true;
     this.error = null;
-    this.user_service.login(this.user_name, this.password)
-      .subscribe(
-        token => {
-          console.log("Result", token);
-          this.router.navigate(this.user_service.state ? [this.user_service.state] : ['/']);
-        },
-        error => {
-          this.error = error.Message;
-          this.processing = false;
-        });
+    this.UserService.login(this.user_name, this.password).subscribe(
+      () => this.Router.navigate([this.UserService.UserState || '/']),
+      error => {
+        this.error = error.Message;
+        this.processing = false;
+      }
+    );
   }
 
   save(model: IUser, isValid: boolean) {
     this.formSubmitted = true;
     // call API to save customer
     if (isValid) {
-      this.user_service.signup(model).subscribe(
-        token => {
-          this.router.navigate(this.user_service.state ? [this.user_service.state] : ['/']);
-        },
+      this.UserService.signup(model).subscribe(
+        () => this.Router.navigate([this.UserService.UserState || '/']),
         error => {
           this.error = error.Message;
           this.processing = false;
-        });
-
+        }
+      );
     }
   }
   reset() {
@@ -64,39 +63,29 @@ export class LoginComponent implements OnInit {
   }
 
   registerFormInit(): FormGroup {
-    return this.fb.group({
-      'Billing_Account_Id': ['', Validators.compose([Validators.required])],
-      'Zip_Code': ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(5), validateInteger])],
-      'User_name': ['', Validators.compose([Validators.required])],
-      'Password': ['', Validators.compose([Validators.required])],
-      'ConfirmPassword': ['', Validators.compose([Validators.required])],
-      'Email_Address': ['', Validators.compose([Validators.required, validateEmail])],
-      'Security_Question_Id': ['none', Validators.compose([Validators.required])],
-      'Security_Question_Answer': ['', Validators.required]
-    }, {validator: equalCheck('Password', 'ConfirmPassword')});
-  }
-
-  // ngOnInit() {
-  //   var res = this.user_service.getSecurityQuestions()
-  //     .subscribe(
-  //       data => {
-  //         this.testdata = data;
-  //         console.log("Data:", this.testdata);
-  //       });
-  //
-  // }
-
-  recPassword($event) {
-    if ($event && $event.preventDefault) {
-      $event.preventDefault();
-    }
-    this.router.navigate(['/login/recover-password']);
+    return this.FormBuilder.group({
+      Billing_Account_Id: ['', Validators.required],
+      Zip_Code: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(5), validateInteger])],
+      User_name: ['', Validators.required],
+      Password: ['', Validators.required],
+      ConfirmPassword: ['', Validators.required],
+      Email_Address: ['', Validators.compose([Validators.required, validateEmail])],
+      Security_Question_Id: ['none', Validators.required],
+      Security_Question_Answer: ['', Validators.required]
+    }, {
+      validator: equalCheck('Password', 'ConfirmPassword')
+    });
   }
 
   ngOnInit(): void {
-    let self = this;
-    self.user_service.getSecurityQuestions().subscribe(response => {
-        this.secQuesArray = response;
-    }, error => this.error = < any > error);
+
+    // Mute the video if the "muted" video tag does not (a supposed FireFox bug.)
+    document.getElementById('login-video-player')['muted'] = 'muted';
+
+    this.UserService.getSecurityQuestions()
+      .subscribe(
+        response => this.secQuesArray = response,
+        error => this.error = <any>error
+      );
   }
 }
