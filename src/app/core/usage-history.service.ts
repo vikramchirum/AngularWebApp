5027456099de497806f700f1f499f1acdbea53f6
Geetha Ramu <geetha.ramu@gexa.gexaenergy.com>
2017-06-23 05:31:23 -0500

    Usage history chart grouped values based on date and showed sum of values by month

--- src/app/core/usage-history.service.ts
+++ src/app/core/usage-history.service.ts
@@ -2,8 +2,9 @@
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 import { HttpClient } from './httpclient';
-import { Observable }  from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import _ from 'lodash';
 
 
 import { UsageHistory } from './models/usage-history.model';
@@ -20,26 +21,34 @@ export class UsageHistoryService {
    * @returns {Observable<UsageHistory[]>}
    */
 
-  getUsageHistory(billingAccountId: number): Observable<UsageHistory[]>   {
-  
+  getUsageHistory(billingAccountId: number): Observable<UsageHistory[]> {
+
     return this.http
-                .get(`/billing_accounts/${billingAccountId}/usage_history`)
-                .map((response: Response) => this.processApiData(response))
-                // {
-                //     // console.log(response.json());
-                //     return <UsageHistory[]> response.json();
-                // })
-                .catch(this.handleError);
+      .get(`/billing_accounts/${billingAccountId}/usage_history`)
+      .map((response: Response) => this.processApiData(response))
+      .catch(this.handleError);
   }
 
-   private processApiData(res: Response) {
+  private processApiData(res: Response) {
     let data = res.json();
-    data.forEach((d) => {
-     //conversion of string to dates.
-    d.Usage_Month = new Date(d.Usage_Month);
-   });   
-    return data;
-  }
+    let groupedByDate = _.groupBy(data, function (item) {
+      return item.Usage_Month;//.substring(0,7);
+    });
+    var aggregateData = _.map(groupedByDate, function (UsageObject, usage_Month) {
+      return {
+        Usage_Month: usage_Month,
+        Usage: _.reduce(UsageObject, function (m, x) {
+          return m + x.Usage;
+        }, 0)
+      };
+    });
+
+    aggregateData.forEach((d) => {
+      //conversion of string to dates.
+      d.Usage_Month = new Date(d.Usage_Month);
+    });
+    return aggregateData;
+    }
 
   private handleError(error: Response) {
     console.log(error.statusText);
