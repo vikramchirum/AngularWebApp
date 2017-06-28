98073f43b3f0afb6ff6131faac91ee9fba6ac64e
James Allen <james.allen@gexaenergy.com>
2017-06-28 10:40:09 -0500

    Adding initialization conditions to prevent null observations/emits. - Handling errors from promises and observables with lodash.

--- src/app/core/BillingAccount.service.ts
+++ src/app/core/BillingAccount.service.ts
@@ -127,7 +127,7 @@ export class BillingAccountService {
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
+      const err = get(body, 'error', JSON.stringify(body));
       errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
