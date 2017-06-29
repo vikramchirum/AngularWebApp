51204815f1b114a4450dc885da6c89f4ebdf72f7
Amanda Breneman <amanda.breneman@gexa.gexaenergy.com>
2017-06-29 17:25:28 -0500

    Added pagination to table and updated graphs

--- src/app/authenticated/controls-and-insights/controls-and-insights.module.ts
+++ src/app/authenticated/controls-and-insights/controls-and-insights.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsModule } from 'ng2-charts';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
-import { SharedModule } from '../../shared/shared.module'
+
+import { SharedModule } from '../../shared/shared.module';
 import { ControlsAndInsightsComponent } from './controls-and-insights.component';
 import { controls_insights_routes } from './controls-and-insights-routing.module';
 import { UsageHistoryComponent } from './usage-history/usage-history.component';
@@ -12,7 +14,8 @@ import { UsageHistoryComponent } from './usage-history/usage-history.component';
     CommonModule,
     controls_insights_routes,
     SharedModule,
-    ChartsModule
+    ChartsModule,
+    PaginationModule.forRoot()
   ],
   declarations: [
     ControlsAndInsightsComponent,
--- src/app/authenticated/controls-and-insights/usage-history/usage-history.component.ts
+++ src/app/authenticated/controls-and-insights/usage-history/usage-history.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { slice } from 'lodash';
+import MockData from 'app/authenticated/payments/payment-history/bills/bills.mock-data.json';
+
 @Component({
   selector: 'mygexa-usage-history',
   templateUrl: './usage-history.component.html',
@@ -7,10 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsageHistoryComponent implements OnInit {
 
-  // Bar Graph
+  /* Bar Graph Properties */
   public barChartOptions: any = {
-    scaleShowVerticalLines: false,
     responsive: true,
+    barThickness: 2,
     tooltips: {
       callbacks: {
         label: (tooltipItem, data) => {
@@ -19,48 +22,92 @@ export class UsageHistoryComponent implements OnInit {
       }
     },
     scales: {
-      yAxes: [{id: 'y-axis-1', type: 'linear', position: 'left', ticks: {min: 0}}]
-    }
+      xAxes: [{
+        display: false
+      }],
+      yAxes: [{
+        display: false,
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+    min: 0
   };
 
-  public barChartLabels: string[] = ['', '', '', '', '', '', ''];
+  public barChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   public barChartType: string = 'bar';
   public barChartLegend: boolean = false;
 
   public barChartData: any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
+    {data: [47, 55, 60, 56, 49, 55, 53, 61, 54, 49, 57, 47, 55, 60, 56, 49, 55, 53, 61, 54, 49, 57, 47, 55], label: 'Series A'}
   ];
 
   public barChartColors: any[] = [
     { backgroundColor: '#959595' }
   ]
 
-  // events
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
-
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
-
-  // Line Graph
+  /* Line Graph Properties */
   public lineChartOptions: any = {
     responsive: true,
     elements: {
       line: {
         tension: 0, // disables bezier curves
       }
+    },
+    tooltips: {
+      callbacks: {
+        label: (tooltipItem, data) => {
+          return tooltipItem.yLabel + 'kwh';
+        }
+      }
+    },
+    scales: {
+      xAxes: [{
+        display: false
+      }],
+      yAxes: [{
+        display: false
+      }]
     }
   };
 
+  public lineChartLegend: boolean = true;
+  public lineChartType: string = 'line';
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  public tableData = MockData;
+  public totalItems: number = MockData.length;
+  public tablePage: number = 1;
+
+  private getEntries(page: number) {
+    const index = (page - 1) * 10;
+    const extent = index + 10;
+    if (extent > MockData.length) {
+      return MockData.slice(index);
+    }
+    return MockData.slice(index, extent);
+  }
+
+  get currentPage() {
+    const pageEntries = this.getEntries(this.tablePage);
+    const dateObj = new Date('2017-06-15T21:19:18.349Z');
+    console.log(dateObj.getFullYear());
+    return pageEntries;
+  }
+
+  public pageChanged(event: any): void {
+    this.tablePage = event.page;
+  }
+
   public lineChartData: Array<any> = [
-    {data: [235, 240, 236, 241, 243, 234, 242], label: '2015'},
-    {data: [234, 242, 237, 238, 241, 237, 240], label: '2016'},
-    {data: [238, 235, 240, 236, 240, 242, 243], label: '2017'}
+    {data: [235, 240, 236, 241, 243, 238, 242, 235, 240, 236, 241, 243, 238], label: '2015', fill: false},
+    {data: [234, 242, 237, 238, 241, 237, 240, 234, 242, 237, 238, 241, 237, 240], label: '2016', fill: false},
+    {data: [238, 235, 240, 236, 240, 242, 243, 238, 235, 240, 236, 240, 242, 243], label: '2017', fill: false}
   ];
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartColors:Array<any> = [
+
+  public lineChartColors: Array<any> = [
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
@@ -86,8 +133,15 @@ export class UsageHistoryComponent implements OnInit {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
   ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 
   constructor() { }
 
