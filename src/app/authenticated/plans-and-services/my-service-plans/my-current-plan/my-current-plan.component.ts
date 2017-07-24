a9508e503b431aa087b7755e369a3895e5fb6dbf
anusha.madabhushi <anusha.madabhushi@gexaenergy.com>
2017-07-24 14:08:25 -0500

    Changes to plans and services view, renew plan card content.

--- src/app/authenticated/plans-and-services/my-service-plans/my-current-plan/my-current-plan.component.ts
+++ src/app/authenticated/plans-and-services/my-service-plans/my-current-plan/my-current-plan.component.ts
@@ -3,6 +3,9 @@ import { ServicePlanUpgradeModalComponent } from 'app/authenticated/plans-and-se
 import {BillingAccountService} from 'app/core/BillingAccount.service';
 import {Subscription} from 'rxjs/Subscription';
 import {BillingAccountClass} from 'app/core/models/BillingAccount.model';
+import {OfferService} from '../../../../core/offer.service';
+import {AllOffersClass} from '../../../../core/models/offers/alloffers.model';
+import {IOffers} from '../../../../core/models/offers/offers.model';
 
 @Component({
   selector: 'mygexa-my-current-plan',
@@ -12,12 +15,18 @@ import {BillingAccountClass} from 'app/core/models/BillingAccount.model';
 export class MyCurrentPlanComponent implements OnInit, AfterViewInit, OnDestroy {
   IsInRenewalTimeFrame: boolean;
   billingAccountSubscription: Subscription;
+  activebillingAccountOffersSubscription: Subscription;
+
+  public All_Offers: AllOffersClass;
+  public FeaturedOffers: AllOffersClass[];
+  public RenewalOffers: IOffers;
+
   selectCheckBox  = false;
   enableSelect = false;
   ActiveBillingAccountDetails: BillingAccountClass;
   @ViewChild('serviceUpgradeModal') serviceUpgradeModal: ServicePlanUpgradeModalComponent;
 
-  constructor(private billingAccount_service: BillingAccountService) {
+  constructor(private billingAccount_service: BillingAccountService, private active_billingaccount_service: OfferService) {
     this.IsInRenewalTimeFrame = false;
   }
 
@@ -26,6 +35,13 @@ export class MyCurrentPlanComponent implements OnInit, AfterViewInit, OnDestroy 
       result => {
         this.ActiveBillingAccountDetails = result;
         this.IsInRenewalTimeFrame = result.IsUpForRenewal;
+      });
+    this.activebillingAccountOffersSubscription = this.active_billingaccount_service.ActiveBillingAccountOfferObservable.subscribe(
+      all_offers => {
+        this.FeaturedOffers = all_offers.filter(item => item.Type === 'Featured_Offers');
+        this.RenewalOffers = (this.FeaturedOffers[0].Offers)[0];
+
+        console.log('Featured_Offers', this.RenewalOffers);
       });
   }
   ngAfterViewInit() {
