f70354941d0a46a43b91950c4b1f5d86ff49872c
vikramrtr <vikram.rtr@gmail.com>
2017-07-28 14:05:15 -0500

    Service Account Refactoring after merge from dev

--- src/app/authenticated/root/root.component.ts
+++ src/app/authenticated/root/root.component.ts
@@ -22,8 +22,7 @@ export class RootComponent implements OnInit, AfterViewInit {
 
   @ViewChild('homeMultiAccountsModal') homeMultiAccountsModal: HomeMultiAccountsModalComponent;
 
-  constructor(private user_service: UserService, private router: Router, private viewContainerRef: ViewContainerRef, private serviceAcctService: ServiceAccountService) { }
-  constructor(private user_service: UserService, private router: Router, private viewContainerRef: ViewContainerRef, private billingAcctService: BillingAccountService) {
+  constructor(private user_service: UserService, private router: Router, private viewContainerRef: ViewContainerRef, private serviceAcctService: ServiceAccountService) {
     this.service_account_length = null;
   }
 
@@ -34,20 +33,24 @@ export class RootComponent implements OnInit, AfterViewInit {
     // this.homeMultiAccountsModal.show();
     if (!this.serviceAcctService.ActiveServiceAccountId) {
       this.homeMultiAccountsModal.show();
-    this.user_service.UserObservable.subscribe(
-      result => { this.service_account_length = result.Account_permissions.length; }
-    );
-    if (!this.serviceAcctService.ActiveBillingAccountId) {
-      if ( this.service_account_length != null && this.service_account_length === 2 ) {
-        this.homeMultiAccountsModal.hideServiceUpgradeModal();
-      } else if ( this.service_account_length != null && this.service_account_length > 2 ) {
-        this.homeMultiAccountsModal.show();
+      this.user_service.UserObservable.subscribe(
+        result => {
+          this.service_account_length = result.Account_permissions.length;
+        }
+      );
+      if (!this.serviceAcctService.ActiveServiceAccountId) {
+        if (this.service_account_length != null && this.service_account_length === 2) {
+          this.homeMultiAccountsModal.hideServiceUpgradeModal();
+        } else if (this.service_account_length != null && this.service_account_length > 2) {
+          this.homeMultiAccountsModal.show();
+        }
       }
     }
   }
+
   ngOnInit() {
     this.user_service.UserObservable.subscribe(
-      result => { this.billing_account_length = result.Account_permissions.length; }
+      result => { this.service_account_length = result.Account_permissions.length; }
     );
     //this.user = this.user_service.logged_in_user;
     //this.user = this.user_service.user_token;
