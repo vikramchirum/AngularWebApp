82feb6aa61fbc68374d0f34b9b4a53b20915da80
James Allen <james.allen@gexaenergy.com>
2017-08-03 15:21:38 -0500

    Preparing for latest.

--- src/app/authenticated/plans-and-services/my-service-plans/my-service-plans.component.ts
+++ src/app/authenticated/plans-and-services/my-service-plans/my-service-plans.component.ts
@@ -3,7 +3,7 @@ import { Component, OnDestroy, ChangeDetectorRef, OnInit, ViewChild } from '@ang
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { Subscription } from 'rxjs/Subscription';
-import { forEach, pull, random, result } from 'lodash';
+import { delay, forEach, pull, random, result } from 'lodash';
 import { ServiceAccountService } from 'app/core/serviceaccount.service';
 import { ServiceAccount } from 'app/core/models/serviceaccount/serviceaccount.model';
 
@@ -18,7 +18,7 @@ export class MyServicePlansComponent implements OnInit, OnDestroy {
   ActiveServiceAccountDetails: ServiceAccount;
   ActiveServiceAccountSubscription: Subscription;
 
-  public doughnutChartDataSet: Observable<any[]> = Observable.of([]);
+  public doughnutChartDataSet: Observable<any[]> = null;
   public doughnutChartColors: any[] = [ { backgroundColor: ['rgba(6,81,128,1.0)', 'rgba(46,177,52,1.0)', 'rgba(254,162,32,1.0)', 'red'] } ];
   public doughnutChartLabels: string[] = ['Time Used', 'Time Remaining', 'Enroll Time', 'Renew Time'];
 
@@ -35,81 +35,85 @@ export class MyServicePlansComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    this.IsInRenewalTimeFrame = false;
-
     this.ActiveServiceAccountSubscription = this.ServiceAccountService.ActiveServiceAccountObservable.subscribe(
       result => {
+
+        this.buildChart(
+          new Date(result.Contract_Start_Date),
+          new Date(),
+          result.Contract_End_Date ? new Date(result.Contract_End_Date) : new Date((new Date()).getTime() + (1000 * 60 * 60 * 24 * 90))
+        );
+
         this.ActiveServiceAccountDetails = result;
         this.IsInRenewalTimeFrame = result.IsUpForRenewal;
         this.ChangeDetectorRef.detectChanges();
       });
 
     this.doughnutChartDataSet = Observable.create(observer => {
-
       this.chartObservers.push(observer);
-
-      observer.next([null, null, null, null]);
-
+      observer.next([]);
       return () => pull(this.chartObservers, observer);
-
     });
 
-    setTimeout(() => this.tempGenerate(), 100);
-
   }
 
-  tempSetDays(): void {
-    const days = prompt('Enter the contract\'s days:', this.chartTotalDays.toString());
-    if (!days) {
-      return;
+  buildChart(
+    Start_Date,
+    Today,
+    End_Date
+  ): void {
+
+    const oneDay = 24 * 60 * 60 * 1000;
+
+    const totalDaysOfContract = Math.round(Math.abs((End_Date.getTime() - Start_Date.getTime()) / (oneDay)));
+    const totalDaysUsed = Math.round(Math.abs((Today.getTime() - Start_Date.getTime()) / (oneDay)));
+    const totalTimeRemaining = Math.round(Math.abs((End_Date.getTime() - Today.getTime()) / (oneDay)));
+
+    console.log(totalDaysOfContract, ' = ', totalDaysUsed, ' + ', totalTimeRemaining);
+
+    const percentage = Math.round((totalTimeRemaining * 100) / totalDaysOfContract);
+
+    const blueBar = Math.round((totalDaysUsed * 100) / (totalDaysOfContract + 90));
+    const greenBar = Math.round((totalTimeRemaining * 100) / (totalDaysOfContract + 90));
+    const yellowBar = Math.round(((totalDaysOfContract * 0.05) * 100) / (totalDaysOfContract + 90));
+    const redBar = Math.round(((totalDaysOfContract * 0.05) * 100) / (totalDaysOfContract + 90));
+
+    forEach(this.chartObservers, observer => {
+      observer.next([
+        blueBar,
+        greenBar,
+        yellowBar,
+        redBar
+      ]);
+    })
+
+    if (!this.gaugeText) {
+      const gaugeText = this.gaugeText.nativeElement.getContext('2d');
+      gaugeText.clearRect(0, 0, 1000, 1000);
+      gaugeText.textAlign = 'center';
+      gaugeText.fillStyle = 'black';
+
+      gaugeText.font = 'bold 150pt sans-serif';
+      gaugeText.fillText(`${percentage}%`, 500, 400);
+
+      gaugeText.font = 'bold 40pt sans-serif';
+      gaugeText.fillText('of Contract Time', 500, 500);
+      gaugeText.fillText('Remaining', 500, 550);
+
+      gaugeText.font = '60pt sans-serif';
+      gaugeText.fillStyle = 'rgba(46,177,52,1.0)';
+      if (percentage < 25) {
+        gaugeText.fillText('Act Now to Lock', 500, 680);
+        gaugeText.fillText('In Savings!', 500, 765);
+      } else if (percentage < 50) {
+        gaugeText.fillText('Time to Start', 500, 680);
+        gaugeText.fillText('Shopping!', 500, 765);
+      } else {
+        gaugeText.fillText('Your Plan is in', 500, 680);
+        gaugeText.fillText('Good Shape!', 500, 765);
+      }
     }
-    const daysNumber = Number(days);
-    if (!daysNumber) {
-      return;
-    }
-    if (daysNumber > 90) {
-      this.chartTotalDays = daysNumber;
-      this.tempGenerate();
-    }
-  }
 
-  tempGenerate(): void {
-
-    const timeUsed = random(1, ((this.chartTotalDays - 90) - 1));
-
-    const timeRemaining = this.chartTotalDays - timeUsed;
-    const timeToEnroll = timeRemaining - 90;
-
-    const percentage = Math.round((timeRemaining * 100) / this.chartTotalDays);
-
-    forEach(this.chartObservers, observer => observer.next([
-      timeUsed, timeToEnroll, 75, 15
-    ]));
-
-    const gaugeText = this.gaugeText.nativeElement.getContext('2d');
-    gaugeText.clearRect(0, 0, 1000, 1000);
-    gaugeText.textAlign = 'center';
-    gaugeText.fillStyle = 'black';
-
-    gaugeText.font = 'bold 150pt sans-serif';
-    gaugeText.fillText(`${percentage}%`, 500, 400);
-
-    gaugeText.font = 'bold 40pt sans-serif';
-    gaugeText.fillText('of Contract Time', 500, 500);
-    gaugeText.fillText('Remaining', 500, 550);
-
-    gaugeText.font = '60pt sans-serif';
-    gaugeText.fillStyle = 'rgba(46,177,52,1.0)';
-    if (percentage < 25) {
-      gaugeText.fillText('Act Now to Lock', 500, 680);
-      gaugeText.fillText('In Savings!', 500, 765);
-    } else if (percentage < 50) {
-      gaugeText.fillText('Time to Start', 500, 680);
-      gaugeText.fillText('Shopping!', 500, 765);
-    } else {
-      gaugeText.fillText('Your Plan is in', 500, 680);
-      gaugeText.fillText('Good Shape!', 500, 765);
-    }
 
   }
 
