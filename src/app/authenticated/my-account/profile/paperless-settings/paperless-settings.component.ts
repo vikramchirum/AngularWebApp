2569460eb48a9f145e4ae29087adb8d034f4c354
Geetha Ramu <geetha.ramu@gexa.gexaenergy.com>
2017-08-17 01:11:09 -0500

    integrated commucication preference with API

--- src/app/authenticated/my-account/profile/paperless-settings/paperless-settings.component.ts
+++ src/app/authenticated/my-account/profile/paperless-settings/paperless-settings.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { forEach, get, isNumber, map, now, random } from 'lodash';
+import { Subscription } from 'rxjs/Subscription';
 
- import {ISearchNotificationOptionRequest} from 'app/core/models/notificationoptions/searchnotificationoptionrequest.model';
- import {AccountType} from 'app/core/models/enums/accounttype';
- import {NotificationType} from 'app/core/models/enums/notificationtype';
- import {NotificationStatus} from 'app/core/models/enums/notificationstatus';
- import {NotificationOptionsService} from 'app/core/notificationoptions.service';
-  
+import { CustomerAccountService } from 'app/core/CustomerAccount.service';
+import { ISearchNotificationOptionRequest } from 'app/core/models/notificationoptions/searchnotificationoptionrequest.model';
+import { AccountType } from 'app/core/models/enums/accounttype';
+import { NotificationType } from 'app/core/models/enums/notificationtype';
+import { NotificationStatus } from 'app/core/models/enums/notificationstatus';
+import { NotificationOptionsService } from 'app/core/notificationoptions.service';
+import { INotificationOption } from 'app/core/models/notificationoptions/notificationoption.model';
+import { CustomerAccount } from 'app/core/models/customeraccount/customeraccount.model';
+
 @Component({
   selector: 'mygexa-paperless-settings',
   templateUrl: './paperless-settings.component.html',
@@ -17,15 +21,22 @@ export class PaperlessSettingsComponent implements OnInit {
 
   sendBillsForm: FormGroup;
   planDocumentsForm: FormGroup;
-  billingOptions = [{ option: 'Email', checked: false }, { option: 'Paper', checked: true }];
-  plansOptions = [{ option: 'Email', checked: false }, { option: 'Paper', checked: true }];
+  billingOptions = [{ option: 'Email', checked: false }, { option: 'Paper', checked: false }];
+  plansOptions = [{ option: 'Email', checked: false }, { option: 'Paper', checked: false }];
   paperlessSettings: boolean = false;
-  goPaperless: boolean = false;  
+  goPaperless: boolean = false;
+  notificationOptions = null;
 
-  searchNotificationOptionRequest: ISearchNotificationOptionRequest = null;
+  searchNotificationOptionRequestForBill: ISearchNotificationOptionRequest = null;
+  searchNotificationOptionRequestForPlans: ISearchNotificationOptionRequest = null;
+  CustomerAccountServiceSubscription: Subscription = null;
+  customerDetails: CustomerAccount = null;
+  updateNotification = null;
 
   constructor(private fb: FormBuilder,
-  private notificationService:NotificationOptionsService) { }
+    private notificationService: NotificationOptionsService,
+    private CustomerAccountService: CustomerAccountService
+  ) { }
 
   ngOnInit() {
     this.sendBillsForm = this.fb.group({
@@ -34,62 +45,112 @@ export class PaperlessSettingsComponent implements OnInit {
     this.planDocumentsForm = this.fb.group({
       plansOptions: this.fb.array([])
     });
+    this.CustomerAccountServiceSubscription = this.CustomerAccountService.CustomerAccountObservable.subscribe(
+      result => {
+        this.customerDetails = result;
+        this.getNotificationOption(this.customerDetails.Id);
+      }
+    );
+  }
 
-    this.searchNotificationOptionRequest = {
-      Account_Info : {
+  getNotificationOption(custId) {
+    this.searchNotificationOptionRequestForBill = {
+      Account_Info: {
         Account_Type: AccountType.GEMS_Residential_Customer_Account,
-       Account_Number: "288673"
+        Account_Number: custId
       },
       Type: NotificationType.Bill,
       Status: NotificationStatus.Active
     }
-    // this.notificationService.searchNotificationOption(this.searchNotificationOptionRequest).subscribe(result =>{
-    //   console.log('Notification Result', result);
-    // })
-    
+    this.searchNotificationOptionRequestForPlans = {
+      Account_Info: {
+        Account_Type: AccountType.GEMS_Residential_Customer_Account,
+        Account_Number: custId
+      },
+      Type: NotificationType.Contract_Expiration,
+      Status: NotificationStatus.Active
+    }
+    this.notificationService.searchNotificationOption(this.searchNotificationOptionRequestForBill).subscribe(result => {
+      console.log('Notification Result for Bill', result);
+      this.notificationOptions = result;
+      if (this.notificationOptions && this.notificationOptions.length > 0) {
+        this.selectedPreference(this.notificationOptions, this.billingOptions);
+      } else {
+        this.billingOptions[1].checked = true;
+      }
+    })
+    this.notificationService.searchNotificationOption(this.searchNotificationOptionRequestForPlans).subscribe(result => {
+      console.log('Notification Result for plans', result);
+      this.notificationOptions = result;
+      if (this.notificationOptions && this.notificationOptions.length > 0) {
+        this.selectedPreference(this.notificationOptions, this.plansOptions);
+      } else {
+        this.plansOptions[1].checked = true;
+      }
+    })
+
+  }
+  selectedPreference(preference, preferenceOptions) {
+    if (preference[0].Paperless) {
+      preferenceOptions[0].checked = preference[0].Paperless;
+    } else {
+      preferenceOptions[1].checked = true;
+    }
   }
 
   togglePaperless(billingOptions, plansOptions) {
     let flag = 1;
-    
+
     billingOptions.forEach(x => {
       if (x.checked) {
-        if(x.option == 'Paper'){
+        if (x.option == 'Paper') {
           flag = 0;
         }
-      }    
+      }
     });
 
-     plansOptions.forEach(x => {
-     if (x.checked) {
-        if(x.option == 'Paper'){
+    plansOptions.forEach(x => {
+      if (x.checked) {
+        if (x.option == 'Paper') {
           flag = 0;
         }
-      }   
+      }
     });
-    if(flag == 1) {
+    if (flag == 1) {
       this.paperlessSettings = true;
-    }else {
-       this.paperlessSettings = false;
+    } else {
+      this.paperlessSettings = false;
     }
+    this.updateNotificationOption();
+  }
   
+  updateNotificationOption() {
+    this.updateNotification = {
+      Type: NotificationType.Bill,
+      Paperless: true,
+      Preferred_Contact_Method: 'Email',
+      Email: this.customerDetails.Email,
+      Status: NotificationStatus.Active,
+      Phone_Number: this.customerDetails.Primary_Phone,
+      Account_Info: {
+        Account_Number: this.customerDetails.Id,
+        Account_Type: AccountType.GEMS_Residential_Customer_Account
+      }
+    }
+    this.notificationService.updateNotificationOption(this.updateNotification).subscribe(
+      () => console.log()),
+      error => {
+        console.log('update notification API error', error.Message);
+      }
 
-    // checkboxOptions.forEach(x => {
-    //   if ((x.option == "Email" || "Paper") && x.checked) {
-    //     this.paperlessSettings = false;
-    //   } else {
-    //     this.paperlessSettings = true;
-    //   }
-    // });
-  
-   
   }
 
+  //To validate if all the elements in array is checked
   validateCheckbox(element, index, array) {
     if (element.checked) {
       return false;
     }
-   return true;
+    return true;
   }
 
   onCheckSelected(option: string, isChecked: boolean, CheckOptions: any) {
