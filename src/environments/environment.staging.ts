701476afacb15ab9ed4b93dbbd0043efc0e39de5
patrick.purcell <patrick.purcell@gexaenergy.com>
2017-08-23 16:54:12 -0500

    Merge branches 'dev' and 'feature/DIGITAL-200-it-should-say-select-instead' of http://neer-bitbucket:7990/scm/gd/my-gexa-2.0 into feature/DIGITAL-200-it-should-say-select-instead
    # Conflicts:
    #	src/app/authenticated/plans-and-services/my-service-plans/change-your-plan/offer-details-popover/offer-details-popover.component.html
    
    Refactored the user service to now use the http client and now it will pass the subscription key to the api (vikram and anusha helped)

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,8 +4,8 @@
 export const environment = {
   Production: true,
   Name: 'Staging',
-  Api_Url: 'https://gexaapidev.azure-api.net/prospecting-admin/api',
-  Api_Token: 'ce6c931123fd4b699f92f53a0fa1fefd',
+  Api_Url: 'https://gexaapidev.azure-api.net/servicing/api',
+  Api_Token: '2f74b90f975e4dc4a93283c6075ebf67',
   Forte_Api_Key: 'GwnI15Vj36',
   Documents_Url: 'https://docsstg.gexaenergy.com',
   DollarAmountFormatter: '1.2-2'
