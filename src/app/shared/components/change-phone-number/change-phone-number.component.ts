f12fe9ead76059d830d792c44ad8fc37977526ce
anusha.madabhushi <anusha.madabhushi@gexaenergy.com>
2017-09-25 16:58:39 -0500

    Integration for phone number section completed.

--- src/app/shared/components/change-phone-number/change-phone-number.component.ts
+++ src/app/shared/components/change-phone-number/change-phone-number.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { validatePhone, equalCheck } from 'app/validators/validator';
+import { CustomerAccountService } from '../../../core/CustomerAccount.service';
+import { CustomerAccountStore } from '../../../core/store/CustomerAccountStore';
+import { Subscription } from 'rxjs/Subscription';
+import { CustomerAccount } from '../../../core/models/customeraccount/customeraccount.model';
+import { IUser } from '../../../core/models/user/User.model';
+import { UserService } from '../../../core/user.service';
 
 @Component({
   selector: 'mygexa-change-phone-number',
   templateUrl: './change-phone-number.component.html',
   styleUrls: ['./change-phone-number.component.scss']
 })
-export class ChangePhoneNumberComponent {
+export class ChangePhoneNumberComponent implements OnInit {
 
   @Output() onCancel: EventEmitter<any> = new EventEmitter();
 
@@ -17,10 +22,29 @@ export class ChangePhoneNumberComponent {
   IsMobileSelected: boolean = null;
   AllowSave: boolean = null;
   checkboxChecked: boolean = null;
+  errorMessage: string = null;
+  IsError: boolean = null;
+  updateUser: IUser = null;
+
+  Exiting_CustomerDetails: CustomerAccount = null;
+  CustomerAccountServiceSubscription: Subscription = null;
   constructor(
-    FormBuilder: FormBuilder
+    private FormBuilder: FormBuilder,
+    private UserService: UserService,
+    private CustomerAccountService: CustomerAccountService,
+    private CustomerAccountStore: CustomerAccountStore
   ) {
-    this.changePhoneNumberForm = FormBuilder.group(
+    this.changePhoneNumberForm = this.changePhoneNumberFormInit();
+  }
+
+  ngOnInit() {
+    this.CustomerAccountServiceSubscription = this.CustomerAccountService.CustomerAccountObservable.subscribe(
+      result => { this.Exiting_CustomerDetails = result; }
+    );
+  }
+
+  changePhoneNumberFormInit(): FormGroup {
+    return this.changePhoneNumberForm = this.FormBuilder.group(
       {
         phone: [null, Validators.compose([Validators.required, validatePhone])],
         confirmPhone: [null, Validators.compose([Validators.required, validatePhone])],
@@ -40,6 +64,26 @@ export class ChangePhoneNumberComponent {
     console.log('valid', this.changePhoneNumberForm.valid);
     if (this.changePhoneNumberForm.valid) {
       /** send form data to api to update in database */
+      const number = this.changePhoneNumberForm.get('phone').value;
+
+      // Format primary phone object for put
+      const area_code = number.substring(0, 3);
+      const phonenumber = number.slice(3);
+
+      // Format existing user object
+      this.Exiting_CustomerDetails.Primary_Phone.Area_Code = area_code;
+      this.Exiting_CustomerDetails.Primary_Phone.Number = phonenumber;
+      this.Exiting_CustomerDetails.Primary_Phone.Type = this.IsMobileSelected ? 'Mobile' : 'Landline';
+      this.Exiting_CustomerDetails.Primary_Phone.Agree_To_Marketing = this.checkboxChecked ? 'true' : 'false';
+
+      // Update user details in weasi
+      this.CustomerAccountStore.UpdateCustomerDetails(this.Exiting_CustomerDetails);
+      this.updateUser = this.UserService.UserCache;
+
+      // Refresh User in Mongo
+      this.UserService.updateUserInMongo(this.updateUser);
+      this.resetForm();
+      this.Exiting_CustomerDetails.Primary_Phone.Type = this.changePhoneNumberForm.get('phone').value;
     }
   }
 
@@ -47,21 +91,30 @@ export class ChangePhoneNumberComponent {
     // const c = this.changePhoneNumberForm.get('mobileRadio').value;
     if (c === 'mobile') {
       this.IsMobileSelected = true;
-      this.AllowSave = this.checkboxChecked ? false : true;
+      // this.AllowSave = this.checkboxChecked ? false : true;
     } else {
       this.IsMobileSelected = false;
-      this.AllowSave = false;
+      // this.AllowSave = false;
     }
   }
 
   mobileCheckBoxChecked(event) {
     if (event.target.checked && this.IsMobileSelected) {
       this.checkboxChecked = true;
-      this.AllowSave =  false;
+      // this.AllowSave =  false;
     } else {
       this.checkboxChecked = false;
-      this.AllowSave =  true;
+      // this.AllowSave =  true;
     }
+  }
+
+  resetForm() {
+    // console.log('Reset form');
+    this.emitCancel();
+    this.IsError = null;
+    this.errorMessage = null;
+    this.submitAttempt = false;
+    this.changePhoneNumberForm = this.changePhoneNumberFormInit();
   }
 
   emitCancel() {
