19c8c3f6a4543b25eed128f30f09b24e112c1434
marirajan.vairamuthu <marirajan.vairamuthu@gexa.gexaenergy.com>
2017-09-29 10:31:06 -0500

    plan card template creation

---
+++ src/app/shared/components/plan-card/plan-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mygexa-plan-card',
+  templateUrl: './plan-card.component.html',
+  styleUrls: ['./plan-card.component.scss']
+})
+export class PlanCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
