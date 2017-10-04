f724ab095b76c1f21ee1f00580554c61ba8d5d0a
anusha.madabhushi <anusha.madabhushi@gexaenergy.com>
2017-10-04 16:54:46 -0500

    Payment extension section Ui and integration completed.

--- src/app/authenticated/payments/payment-options/payment-extension/payment-extension.component.ts
+++ src/app/authenticated/payments/payment-options/payment-extension/payment-extension.component.ts
@@ -1,31 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-
-
-// TODO: remove this once we're getting the user's "extension requested status" from the API.
-let temp_requestedExtension = false;
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { ServiceAccount } from '../../../../core/models/serviceaccount/serviceaccount.model';
+import { ServiceAccountService } from '../../../../core/serviceaccount.service';
+import { PaymentExtensionService } from '../../../../core/payment-extension.service';
+import { IPaymentExtension } from '../../../../core/models/paymentextension/payment-extension.model';
+import { Subscription } from 'rxjs/Subscription';
+import { ExtensionStatus } from '../../../../core/models/enums/extensionstatus';
 
 @Component({
   selector: 'mygexa-payment-extension',
   templateUrl: './payment-extension.component.html',
   styleUrls: ['./payment-extension.component.scss']
 })
-export class PaymentExtensionComponent implements OnInit {
-
+export class PaymentExtensionComponent implements OnInit, OnDestroy {
+  PaymentExtensionResponseStatus: ExtensionStatus;
+  customer_On_Payment_Extension: boolean = null;
+  paymentExtension: IPaymentExtension = null;
+  paymentExtensionServiceSubscription: Subscription = null;
+  serviceAccountDetailsSubscription: Subscription = null;
+  serviceAccountDetails: ServiceAccount;
   pastDueAmount: number = null;
   requestedExtension: boolean = null;
-  requestedExtensionRecently: boolean = null;
-
-  constructor(
+  extensionSuccessfull: boolean = null;
+  constructor(private activeServiceAccount: ServiceAccountService,
+              private paymentExtensionService: PaymentExtensionService
   ) {}
 
   ngOnInit() {
-
+    this.serviceAccountDetailsSubscription  = this.activeServiceAccount.ActiveServiceAccountObservable.subscribe(
+      ActiveServiceAccountDetails => { this.serviceAccountDetails = ActiveServiceAccountDetails;
+      this.pastDueAmount = ActiveServiceAccountDetails.Past_Due;  console.log('paste due', this.pastDueAmount); }
+    );
   }
 
   requestExtension(): void {
-    // TODO: post data to the API.
-    this.requestedExtension = temp_requestedExtension = true;
-    this.requestedExtensionRecently = true;
+    this.requestedExtension = true;
+    this.paymentExtensionServiceSubscription = this.paymentExtensionService.getPaymentExtensionStatus(this.serviceAccountDetails.Id).subscribe(
+      paymentExtensionStatus => {
+        this.paymentExtension = paymentExtensionStatus;
+        if (paymentExtensionStatus.Status === String(ExtensionStatus.SUCCESSFUL)) {
+          this.extensionSuccessfull = true;
+        } else {
+          this.extensionSuccessfull = true;
+        }
+      }
+    );
+  }
+  cancelExtension() {
+    console.log('hi cancelled');
+    this.requestedExtension = false;
+    this.paymentExtension = null;
+    this.extensionSuccessfull = false;
+  }
+
+  ngOnDestroy() {
+    this.serviceAccountDetailsSubscription.unsubscribe();
+    if (this.paymentExtensionServiceSubscription) {
+      this.paymentExtensionServiceSubscription.unsubscribe();
+    }
   }
 
 }
