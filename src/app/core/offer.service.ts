4476fcf8ebe4b409695c8ef125f710d1b43bbccd
patrick.purcell <patrick.purcell@gexaenergy.com>
2017-10-09 17:38:09 -0500

    Changed upgrade offers to pull from new method on the api

--- src/app/core/offer.service.ts
+++ src/app/core/offer.service.ts
@@ -1,17 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { clone, forEach, pull, map } from 'lodash';
-import { HttpClient } from './httpclient';
-import { AllOffersClass, UpgradeOffersClass } from './models/offers/alloffers.model';
-import { ServiceAccountService } from './serviceaccount.service';
-import { IOffers } from './models/offers/offers.model';
+import {clone, forEach, pull, map} from 'lodash';
+import {HttpClient} from './httpclient';
+import {AllOffersClass, UpgradeOffersClass} from './models/offers/alloffers.model';
+import {ServiceAccountService} from './serviceaccount.service';
+import {IOffers} from './models/offers/offers.model';
 
 
 @Injectable()
 export class OfferService {
-
 
 
   constructor(private http: HttpClient, private serviceAccountService: ServiceAccountService) {
@@ -21,7 +20,10 @@ export class OfferService {
     console.log('Offer params', offer);
     return this.http
       .get(`/v2/Offers?option.startDate=${offer.startDate}&option.plan.tDU.duns_Number=${offer.dunsNumber}&option.approved=${offer.approved}&option.pageSize=${offer.page_size}&option.channel_Id=${offer.channelId}`)
-      .map(data => { data.json(); return data.json(); })
+      .map(data => {
+        data.json();
+        return data.json();
+      })
       .map(data => <IOffers[]>data['Items'])
       .catch(error => this.http.handleHttpError(error));
   }
@@ -29,17 +31,18 @@ export class OfferService {
   getRenewalOffers(ActiveServiceAccountId: string): Observable<AllOffersClass[]> {
     return this.http.get(`/service_accounts/${ActiveServiceAccountId}/offers`)
       .map(data => data.json())
-      .map(data => { map(data, OffersData => new AllOffersClass(OffersData));
-                      // console.log('Offers', data);
-                      return data; })
+      .map(data => {
+        map(data, OffersData => new AllOffersClass(OffersData));
+        // console.log('Offers', data);
+        return data;
+      })
       .catch(error => this.http.handleHttpError(error));
   }
 
-  getUpgradeOffers(ActiveServiceAccount_CuurentOffer_Term: number, ActiveServiceAccount_TDU_DUNS_Number: string): Observable<IOffers[]> {
-    return this.http.get(`/v2/Offers?option.approved=true&option.startDate=${new Date().toISOString()}&option.plan.term_Greater_Than=
-    ${ActiveServiceAccount_CuurentOffer_Term}&option.plan.tDU.duns_Number=${ActiveServiceAccount_TDU_DUNS_Number}&option.pageSize=40`)
+  getUpgradeOffers(ActiveServiceAccountId: string): Observable<IOffers[]> {
+    return this.http.get(`/service_accounts/${ActiveServiceAccountId}/upgrades`)
       .map(data => { data.json(); return data.json(); })
-      .map(data => <IOffers[]>data['Items'])
+      .map(data => <IOffers[]>data)
       .catch(error => this.http.handleHttpError(error));
   }
 
@@ -47,7 +50,10 @@ export class OfferService {
     return this.http
       .get(`/v2/Offers?option.promotion.code=${promoCode}&option.approved=true&option.startDate=
     ${new Date().toISOString()}&option.plan.tDU.duns_Number=${ActiveServiceAccount_TDU_DUNS_Number}`)
-      .map(data => { data.json(); return data.json(); })
+      .map(data => {
+        data.json();
+        return data.json();
+      })
       .map(data => <IOffers[]>data['Items'])
       .catch(error => this.http.handleHttpError(error));
   }
@@ -55,14 +61,22 @@ export class OfferService {
   getLyricOfferDetails(ActiveServiceAccount_TDU_DUNS_Number: string) {
     return this.http.get(`/v2/Offers?option.approved=true&option.startDate=
     ${new Date().toISOString()}&option.promotion.code=LYRIC&option.plan.tDU.duns_Number=${ActiveServiceAccount_TDU_DUNS_Number}`)
-      .map(data => { data.json(); console.log('GexaLyricOffer', data.json()); return data.json(); })
+      .map(data => {
+        data.json();
+        console.log('GexaLyricOffer', data.json());
+        return data.json();
+      })
       .map(data => <IOffers[]>data['Items'])
       .catch(error => this.http.handleHttpError(error));
   }
 
   getOffersByChannel(Channel_Id: string): Observable<IOffers[]> {
     return this.http.get(`/v2/Offers?ov2/Offers?option.channel_Id=${Channel_Id}`)
-      .map(data => { data.json(); console.log('Channel offers', data.json()); return data.json(); })
+      .map(data => {
+        data.json();
+        console.log('Channel offers', data.json());
+        return data.json();
+      })
       .map(data => <IOffers[]>data['Items'])
       .catch(error => this.http.handleHttpError(error));
   }
