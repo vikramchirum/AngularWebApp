752fd2a5c5e3e1258f1623f661040e2a9653fa0f
anusha.madabhushi <anusha.madabhushi@gexaenergy.com>
2017-11-08 12:09:54 -0600

    Removed console logs

--- src/app/validators/validator.ts
+++ src/app/validators/validator.ts
@@ -111,7 +111,6 @@ export function validateInteger(c: FormControl) {
 
 export function validateEmail(c: FormControl) {
   const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  console.log('value', EMAIL_REGEXP.test(c.value));
   return EMAIL_REGEXP.test(c.value) ? null : {
     validateEmail: {
       valid: false
