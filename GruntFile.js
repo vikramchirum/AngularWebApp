91c91f925b151505bfbea1509737a97df2b0ad68
patrick.purcell <patrick.purcell@gexaenergy.com>
2017-11-15 10:16:16 -0600

    Removed unused video files Added new environment file for octopus deployments Added steps to include web.config, bump the version number and push to octopus

--- GruntFile.js
+++ GruntFile.js
@@ -1,20 +1,48 @@
 /**
  * Created by patrick.purcell on 6/2/2017.
  */
-module.exports = function (grunt) {
+module.exports = function ( grunt ) {
 
-  var path = grunt.option('deploypath');
+  var path = grunt.option( 'deploypath' );
 
-  grunt.initConfig({
-     copy:{
-       deploy: {
-         expand: true,
-         cwd: 'dist',
-         src: '**',
-         dest: path
-       }
-     }
-  });
+  grunt.initConfig( {
+    clean: {
+      build: [ './pkg/**/*' ]
+    },
+    "octo-pack": {
+      prod: {
+        options: {
+          dst: './pkg'
+        },
+        cwd: './dist',
+        src: [ '**/*']
+      }
+    },
+    "octo-push": {
+      options: {
+        host: 'http://octopus',
+        apiKey: 'API-FLRBYNHIX14CXBV82R6CH7BGYEY',
+        replace: true
+      },
+      src: [ './pkg/**/*' ]
+    },
+    copy: {
+      config:{
+        expand: true,
+        cwd: 'publish',
+        src: 'web.config',
+        dest: './dist'
+      },
+      deploy: {
+        expand: true,
+        cwd: 'dist',
+        src: '**',
+        dest: path
+      }
+    }
+  } );
 
-  require('load-grunt-tasks')(grunt);
+  grunt.registerTask('publish',  ['bump-only', 'copy:config', 'clean', 'octo-pack:prod', 'octo-push']);
+
+  require( 'load-grunt-tasks' )( grunt );
 };
