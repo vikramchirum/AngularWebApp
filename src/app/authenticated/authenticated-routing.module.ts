25c5d56372f807d97b8197a410162cbe32c0d456
Ryan Purcell <ryan.purcell@gexaenergy.com>
2017-12-08 16:19:42 -0600

    Replaced all load children function references with the string location of the actual modules

--- src/app/authenticated/authenticated-routing.module.ts
+++ src/app/authenticated/authenticated-routing.module.ts
@@ -33,15 +33,15 @@ const routes: Route[] = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home-multi', component: HomeMultiAccountsModalComponent },
-      { path: 'home', loadChildren: loadHomeModule },
+      { path: 'home', loadChildren: './home/home.module#HomeModule' },
       { path: 'account', redirectTo: 'account/profile', pathMatch: 'full' },
-      { path: 'account', loadChildren: loadMyAccountModule },
+      { path: 'account', loadChildren: './my-account/my-account.module#MyAccountModule' },
       { path: 'payments', redirectTo: 'payments/view-my-bill', pathMatch: 'full' },
-      { path: 'payments', loadChildren: loadPaymentModule },
+      { path: 'payments', loadChildren: './payments/payments.module#PaymentsModule' },
       { path: 'plans-and-services', redirectTo: 'plans-and-services/my-services-plans', pathMatch: 'full' },
-      { path: 'plans-and-services', loadChildren: loadPlansAndServicesModule },
+      { path: 'plans-and-services', loadChildren: './plans-and-services/plans-and-services.module#PlansAndServicesModule' },
       { path: 'controls-and-insights', redirectTo: 'controls-and-insights/usage-history', pathMatch: 'full' },
-      { path: 'controls-and-insights', loadChildren: loadControlInsightsModule }
+      { path: 'controls-and-insights', loadChildren: './controls-and-insights/controls-and-insights.module#ControlsAndInsightsModule' }
     ]
   }
 ];
