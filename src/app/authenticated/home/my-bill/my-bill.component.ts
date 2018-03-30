b4fd171ef1494296de2ab2d06efd1f04fa716a41
Suneel Nangineni <suneel.nangineni@gexaenergy.com>
2018-03-29 23:51:55 -0400

    Merge pull request #461 in GD/mygexa.gexaenergy.com from hotfix/fix-critical-issues-for-Go-Live to release/1.4.0
    Squashed commit of the following:
    
    commit e5a7cbcde58cd01dec624e90716505b4a16f412d
    Author: vikramchirumamilla <vikram.chirumamilla@gexaenergy.com>
    Date:   Thu Mar 29 22:45:21 2018 -0500
    
        Fix ViewBill Date on Home Page

--- src/app/authenticated/home/my-bill/my-bill.component.ts
+++ src/app/authenticated/home/my-bill/my-bill.component.ts
@@ -43,6 +43,7 @@ export class MyBillComponent implements OnInit, OnDestroy {
   dueDate: Date = null;
   public Payments: PaymentsHistory[] = null;
   LatestBillAmount: number;
+  LatestBillPaymentDate: Date;
   ScheduledAutoBillPaymentDate: Date = null;
   showDueDate: boolean = null;
   PaymentsLength: number = null;
@@ -84,6 +85,7 @@ export class MyBillComponent implements OnInit, OnDestroy {
                       this.paymentStatus = this.Payments[0].PaymentStatus;
                       if (this.paymentStatus === 'In Progress' || this.paymentStatus === 'Cleared') {
                         this.LatestBillAmount = this.Payments[0].PaymentAmount;
+                        this.LatestBillPaymentDate = this.Payments[0].PaymentDate;
                         this.showDueDate = false;
                       } else {
                         this.showDueDate = true;
