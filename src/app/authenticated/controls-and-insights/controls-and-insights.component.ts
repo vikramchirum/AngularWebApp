0c45f3c8b9faeab783b67638f1f881726b2348ed
Larry Parks <larry.parks@gexaenergy.com>
2018-06-29 16:39:10 -0500

    Power usage tracker and daily usage

--- src/app/authenticated/controls-and-insights/controls-and-insights.component.ts
+++ src/app/authenticated/controls-and-insights/controls-and-insights.component.ts
@@ -5,9 +5,11 @@ import { Subscription } from 'rxjs/Subscription';
 import { UsageHistoryService } from 'app/core/usage-history.service';
 import { ServiceAccountService } from 'app/core/serviceaccount.service';
 import { ServiceAccount } from 'app/core/models/serviceaccount/serviceaccount.model';
-import { result, startsWith } from 'lodash';
+import { result, startsWith, takeRight, values } from 'lodash';
 import { Router } from '@angular/router';
+import { UsageComparison } from '../../core/models/usage/usage-comparison.model';
 
+import * as moment from 'moment';
 
 @Component({
   selector: 'mygexa-controls-and-insights',
@@ -25,6 +27,51 @@ export class ControlsAndInsightsComponent implements OnDestroy {
   public tablePage = 1;
   public tableData: any[] = [];
 
+  /* Usage Tracker line chart */
+  public dailyUsage: UsageComparison;
+  public cycleDates = [];
+
+  private monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'];
+  /* Line Graph Properties */
+  public lineChartOptions: any = {
+    responsive: true,
+    elements: {
+      line: {
+        tension: 0.1,
+        fill: true
+      }
+    },
+    tooltips: {
+      callbacks: {
+        label: (tooltipItem) => {
+          return Math.round(tooltipItem.yLabel) + ' kWh';
+        }
+      }
+    },
+    scales: {
+      xAxes: [{
+        display: true
+      }],
+      yAxes: [{
+        display: true,
+        ticks: {
+          beginAtZero: true,
+          callback: function(value, index, values) {
+            return `${value} kWh`;
+          }
+        }
+      }]
+    }
+  };
+  public lineChartData = [];
+  public lineChartColors: Array<any> = [
+    { borderColor: 'rgba(6,81,128,1.0)', backgroundColor: 'rgba(6,81,128,0.5)' },
+    { borderColor: 'rgba(254,162,32,1.0)', backgroundColor: 'rgba(254,162,32,0.5)' },
+    { borderColor: 'rgba(46,177,52,1.0)', backgroundColor: 'rgba(46,177,52,0.5)' },
+    { borderColor: 'rgba(27,141,205,1.0)', backgroundColor: 'rgba(27,141,205,0.5)' }
+  ];
+
   constructor(
     private usageHistoryService: UsageHistoryService,
     private ServiceAccountService: ServiceAccountService,
@@ -35,6 +82,7 @@ export class ControlsAndInsightsComponent implements OnDestroy {
       activeServiceAccount => {
         this.activeServiceAccount = activeServiceAccount;
         this.getUsageHistoryByServiceAccountId();
+        this.getUsageComparisonByUAN();
       }
     );
   }
@@ -73,6 +121,72 @@ export class ControlsAndInsightsComponent implements OnDestroy {
           this.isDataAvailable = true;
         });
     }
+  }
+
+  getUsageComparisonByUAN() {
+    if (this.activeServiceAccount) {
+      this.usageHistoryService.getUsageComparison(this.activeServiceAccount.UAN)
+        .subscribe(dailyUsage => {
+          this.dailyUsage = dailyUsage;
+          this.populateChart(dailyUsage.Daily_Usage_List);
+        });
+    }
+  }
+
+  populateChart(usageHistory: any[]): void {
+
+    const datagroups = {};
+    let tempMonth: number;
+
+    const currentMonthUsageData = usageHistory.filter(day => {
+      const cycleStart = new Date(this.dailyUsage.Meter_Read_Cycles[1].Start_Date);
+      const cycleEnd = new Date(this.dailyUsage.Meter_Read_Cycles[1].End_Date);
+      const usageDay = new Date(day.Date);
+      if (usageDay >= cycleStart && usageDay <= cycleEnd) {
+        this.cycleDates.push(`${usageDay.getMonth() + 1}/${usageDay.getDate()}`);
+        return day;
+      }
+    });
+    
+    for (let i = 0; i < currentMonthUsageData.length; i++) {
+      const usageDate = new Date(currentMonthUsageData[i].Date);
+      tempMonth = usageDate.getMonth();
+
+      if (!datagroups[tempMonth]) {
+        datagroups[tempMonth] = { data: [], label: this.monthNames[tempMonth] };
+      }
+      datagroups[tempMonth].data.push(currentMonthUsageData[i].Usage);
+    }
+
+    const dataToDisplay = takeRight(values(datagroups));
+
+    while (this.lineChartData.length) {
+      this.lineChartData.pop();
+    }
+
+    if (dataToDisplay && dataToDisplay.length > 0) {
+      this.lineChartData.push(dataToDisplay);
+    }
+
+    const cycleStart = new Date(this.dailyUsage.Meter_Read_Cycles[1].Start_Date);
+    const cycleEnd = new Date(this.dailyUsage.Meter_Read_Cycles[1].End_Date);
+    this.getDates(cycleStart, cycleEnd);
+
+    this.isDataAvailable = true;
+
+  }
+
+  private getDates(startDate: Date, endDate: Date): void {
+    let dates = [];
+    let currentDate = moment(startDate);
+    let stopDate = moment(endDate);
+    
+    while (currentDate <= stopDate) {
+      dates.push(`${currentDate.format("M/DD")}`);
+      currentDate = moment(currentDate).add(1, 'days');
+    }
+
+    this.cycleDates = dates;
   }
 
 }
--- src/app/authenticated/controls-and-insights/power-usage-tracker/power-usage-tracker.component.ts
+++ src/app/authenticated/controls-and-insights/power-usage-tracker/power-usage-tracker.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
 
 import { Subscription } from 'rxjs/Subscription';
+import { takeRight, values } from 'lodash';
 import { ServiceAccountService } from 'app/core/serviceaccount.service';
 import { ServiceAccount } from 'app/core/models/serviceaccount/serviceaccount.model';
 import { UsageHistoryService } from '../../../core/usage-history.service';
 import { UsageComparison } from '../../../core/models/usage/usage-comparison.model';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'mygexa-power-usage-tracker',
@@ -16,12 +18,16 @@ export class PowerUsageTrackerComponent implements OnDestroy {
   private activeServiceAccount: ServiceAccount = null;
   private ServiceAccountSubscription: Subscription = null;
 
+  private comparisonStartDate: Date;
+  private comparisonEndDate: Date;
+
   public usageComparison: UsageComparison;
   public usagePrediction: UsageComparison;
 
-  public percentageDifference: number;
-  public isDataAvailable: boolean = false;
+  public percentageIncrease: number;
   public daysInLastCycle: number;
+  public remainingCycleDays: number;
+  public isDataAvailable: boolean = false;
   
   constructor(
     private ServiceAccountService: ServiceAccountService,
@@ -31,8 +37,13 @@ export class PowerUsageTrackerComponent implements OnDestroy {
       activeServiceAccount => {
         this.activeServiceAccount = activeServiceAccount;
         this.getUsageComparisonByUAN();
+        this.isDataAvailable = true;
       }
     )
+  }
+
+  ngOnDestroy() {
+    this.ServiceAccountSubscription.unsubscribe();
   }
 
   getUsageComparisonByUAN() {
@@ -41,8 +52,7 @@ export class PowerUsageTrackerComponent implements OnDestroy {
         .subscribe(usageComparison => {
           this.usageComparison = usageComparison;
           this.getUsagePredictionForCurrentMonth(usageComparison.Meter_Read_Cycles[1].End_Date);
-          this.calculateDaysInLastCycle();
-          this.isDataAvailable = true;
+          this.completeCalculations();
         });
     }
   }
@@ -52,65 +62,80 @@ export class PowerUsageTrackerComponent implements OnDestroy {
       this.usageHistoryService.getUsagePrediction(this.activeServiceAccount.UAN, endDate).subscribe(usagePreduction => {
         this.usagePrediction = usagePreduction;
         this.calculatePercentDifference();
+        this.remainingCycleDays = this.usagePrediction.Days_InTo_Current_Cycle - this.usageComparison.Days_InTo_Current_Cycle;
       });
     }
   }
 
-  getStyle(number: number): string {
-    if (number < 0) {
-      return 'red';
+  getPercentStyle(number: number): string {
+    if (number <= 0) {
+      return '#2eb134';
     } else {
-      return '#2eb134'; // green
+      return 'red'; // green
     }
   }
 
-  getBgColor(): string {
-    if (this.usageComparison.Meter_Read_Cycles[0].Usage < this.usageComparison.Meter_Read_Cycles[1].Usage) {
-      return 'red';
+  getKwhStyle(): string {
+    const pastUsage = this.usageComparison.Meter_Read_Cycles[0].Usage;
+    const estimatedUsage = this.usagePrediction.Meter_Read_Cycles[0].Usage;
+    if (pastUsage > estimatedUsage) {
+      return '#2eb134';
     } else {
-      return '#2eb134'; // green
+      return 'red';
     }
   }
 
-  getWidth(cycleIndex: number): string {
-    switch (cycleIndex) {
-      case 0:
-        if (this.usageComparison.Meter_Read_Cycles[0].Usage < this.usageComparison.Meter_Read_Cycles[1].Usage) {
-          return '55%';
-        } else {
-          return '85%';
-        }
-      case 1:
-        if (this.usageComparison.Meter_Read_Cycles[0].Usage < this.usageComparison.Meter_Read_Cycles[1].Usage) {
-          return '85%';
-        } else {
-          return '55%';
-        }
+  getRemainingDaysWidth(): string {
+    const daysInCurrentCycle = this.usagePrediction.Days_InTo_Current_Cycle;
+    if (daysInCurrentCycle < this.daysInLastCycle) {
+      return '28%';
+    } else if (daysInCurrentCycle > this.daysInLastCycle) {
+      return '32%';
+    } else {
+      return '30%';
     }
   }
 
-  ngOnDestroy() {
-    this.ServiceAccountSubscription.unsubscribe();
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
   }
 
   private calculatePercentDifference(): void {
-    const diff = this.usageComparison.Meter_Read_Cycles[1].Usage / this.usagePrediction.Meter_Read_Cycles[0].Usage;
-    if (isNaN(diff)) {
-      this.percentageDifference = 0;
+    let previousUsageWithinComparisonRange = this.usageComparison.Daily_Usage_List.filter(day => {
+      let date = new Date(day.Date);
+      if (date >= this.comparisonStartDate && date < this.comparisonEndDate)
+        return day;
+    });
+    let previousUsageInComparisonTotal = previousUsageWithinComparisonRange.map(day => {
+      return day.Usage;
+    }).reduce((a, b) => a + b, 0);
+
+    const currentUsage = this.usageComparison.Meter_Read_Cycles[1].Usage;
+    const increase = currentUsage - previousUsageInComparisonTotal;
+    const increaseRatio = increase / previousUsageInComparisonTotal;
+
+    if (isNaN(increaseRatio)) {
+      this.percentageIncrease = 0;
     } else {
-      if (diff < 100) {
-        this.percentageDifference = Math.round(diff * 100);
-      } else {
-        this.percentageDifference = Math.round(-Math.abs(diff * 100 - 100));
-      }
+      this.percentageIncrease = increaseRatio * 100;
     }
   }
 
-  private calculateDaysInLastCycle(): void {
+  private completeCalculations(): void {
     const date2 = new Date(this.usageComparison.Meter_Read_Cycles[0].End_Date);
     const date1 = new Date(this.usageComparison.Meter_Read_Cycles[0].Start_Date);
     const timeDiff = Math.abs(date2.getTime() - date1.getTime());
     this.daysInLastCycle = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    const daysIntoCycle = this.usageComparison.Days_InTo_Current_Cycle;
+    let endDate = new Date(date1);
+    endDate.setDate(endDate.getDate() + daysIntoCycle);
+    this.comparisonEndDate = endDate;
+    this.comparisonStartDate = date1;
   }
 
 }
