73bcd01914e249b925048190786a6b97764b77c5
Dug Sohn <doug.sohn@gexaenergy.com>
2018-07-04 18:05:58 -0400

    Cleaned up Show Hide for Bill and Ebill, and added a close.

--- src/app/guest/login/login-register-modal/login-register-modal.component.ts
+++ src/app/guest/login/login-register-modal/login-register-modal.component.ts
@@ -207,7 +207,7 @@ export class LoginRegisterModalComponent implements OnInit {
   
   ServiceNumberEBillToggle(state) {
     
-    this.showServiceNumberEBill = !this.showServiceNumberEBill;
+    this.showServiceNumberEBill = state;
   }
   
 }
