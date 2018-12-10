de7159f437c1676d825034f16bf63161f9cee2fa
Larry Parks <larry.parks@gexaenergy.com>
2018-12-10 16:53:01 -0600

    Adjustded invoice due date to match GEMS

--- src/app/core/invoiceservice.service.ts
+++ src/app/core/invoiceservice.service.ts
@@ -29,7 +29,7 @@ export class InvoiceService {
       .map(res => res.json())
       .map(invoices => forEach(invoices, invoice => {
         invoice.Invoice_Date = new Date(invoice.Invoice_Date).setDate(new Date(invoice.Invoice_Date).getDate()+1); // Add day to match GEMS
-        invoice.Due_Date = new Date(invoice.Due_Date);
+        invoice.Due_Date = new Date(invoice.Due_Date).setDate(new Date(invoice.Due_Date).getDate()+1); // Add day to match GEMS
       }))
       .map(invoices => this.cachedInvoices = invoices)
       .catch(error => this.HttpClient.handleHttpError(error));
